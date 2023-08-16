a57da8aedf028695372fa753fc5cd618eb524229
Nyakana <nyakanaadam@gmail.com>
2023-08-16 13:32:48 +0300

    Adam

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById(menuList);
+menuList.style.maxHeight == "0px";
+function toggleMenu(){
+    if (menuList.style.maxHeight == "0px"){
+        menuList.style.maxHeight = "100vh";
+    }
+    else{
+        menuList.style.maxHeight = "0px";
+    }
+}
